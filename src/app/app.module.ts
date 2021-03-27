c1b9c279dd058f3e69c7e88c4074782633fe7660
Jacob Ian Matthews <jacob@jacobian.com.au>
2021-03-27 14:42:38 +0800

    feat(Info): Add components to App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UniverseComponent } from './universe/universe.component';
+import { InfoComponent } from './universe/info/info.component';
+import { InfoBoxComponent } from './universe/info-box/info-box.component';
 
 @NgModule({
-  declarations: [AppComponent, UniverseComponent],
+  declarations: [AppComponent, UniverseComponent, InfoComponent, InfoBoxComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
